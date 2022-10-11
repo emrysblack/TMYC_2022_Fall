c91b1406bbe7e571a29c3840e509c0d6f7fa618c
Brady Field <emrysblack@gmail.com>
2022-10-10 22:37:49 -0500

    week 2 main challenge

---
+++ week2/main.py
@@ -0,0 +1,114 @@
+from utils.csv_reader import import_csv_flat
+
+
+def trace_wire(path):
+    coordinates = [(0,0)]
+    directions = {"U":(0,1), "D":(0,-1), "R":(1,0), "L":(-1,0)}
+
+    for segment in path:
+        last_position = coordinates[-1]
+        direction = directions[segment[0]]
+        magnitude = int(segment[1:])
+        next_position = (last_position[0]+direction[0] * magnitude, 
+                         last_position[1]+direction[1] * magnitude)
+        coordinates.append(next_position)
+    return coordinates
+
+def find_crossings(wire1, wire2):
+    crossings = []
+
+    # much more complicated working with lines than points, but also much faster
+    for i in range(len(wire1)-1):
+        for j in range(len(wire2)-1):
+            # find low, mid, high points
+            if wire1[i][0] == wire1[i+1][0]: # first line vertical
+                mid1 = wire1[i][0]
+                low_high1 = [wire2[j][0], wire2[j+1][0]]
+                low_high1.sort()
+                mid2 = wire2[j][1]
+                low_high2 = [wire1[i][1], wire1[i+1][1]]
+                low_high2.sort()
+                if (mid1 >= low_high1[0] and mid1 <= low_high1[1] and
+                    mid2 >= low_high2[0] and mid2 <= low_high2[1]):
+                    crossings.append((mid1,mid2))
+            else: # first line horizontal
+                mid1 = wire1[i][1]
+                low_high1 = [wire2[j][1], wire2[j+1][1]]
+                low_high1.sort()
+                mid2 = wire2[j][0]
+                low_high2 = [wire1[i][0], wire1[i+1][0]]
+                low_high2.sort()
+                if (mid1 >= low_high1[0] and mid1 <= low_high1[1] and
+                    mid2 >= low_high2[0] and mid2 <= low_high2[1]):
+                    crossings.append((mid2,mid1))
+
+    return crossings[1:]
+
+
+def get_distance_manhattan(coord, origin=None):
+    if origin is None:
+        origin=(0,0)
+    # pair coordinates with origin
+    x,y = ([origin[0],coord[0]],[origin[1],coord[1]])
+
+    # reorder for max/min
+    x.sort()
+    y.sort()
+
+    # get absolute distance
+    return x[1] - x[0] + y[1] - y[0]
+
+def get_intersection_steps(intersection, wire1, wire2):
+
+    # is point on the line?
+    def contains(point, start, end):
+        x = [start[0], end[0]]
+        y = [start[1], end[1]]
+        x.sort()
+        y.sort()
+        if start[0] == end[0]:
+            return point[0] == start[0] and point[1] >= y[0] and point[1] <= y[1]
+        return point[1] == start[1] and point[0] >= x[0] and point[0] <= x[1]
+    
+    steps = 0
+
+    # steps from wire1
+    for i in range(len(wire1)-1):
+        if contains(intersection, wire1[i], wire1[i+1]):
+            steps += get_distance_manhattan(intersection, wire1[i])
+            break
+        steps += get_distance_manhattan(wire1[i], wire1[i+1])
+    
+    # steps from wire2
+    for i in range(len(wire2)-1):
+        if contains(intersection, wire2[i], wire2[i+1]):
+            steps += get_distance_manhattan(intersection, wire2[i])
+            break
+        steps += get_distance_manhattan(wire2[i], wire2[i+1])
+
+    return steps
+
+def part1():
+    wire1 = trace_wire(import_csv_flat('week2/wire1.csv'))
+    wire2 = trace_wire(import_csv_flat('week2/wire2.csv'))
+
+    crossings = find_crossings(wire1, wire2)
+    crossings.sort(key=lambda x: get_distance_manhattan(x))
+    return get_distance_manhattan(crossings[0])
+
+def part2():
+    wire1 = trace_wire(import_csv_flat('week2/wire1.csv'))
+    wire2 = trace_wire(import_csv_flat('week2/wire2.csv'))
+
+    crossings = find_crossings(wire1, wire2)
+    steps = [get_intersection_steps(step, wire1, wire2) for step in crossings]
+    steps.sort()
+
+    return steps[0]
+
+def main():
+    print("part 1: ", part1())
+    print("part 2: ", part2())
+
+if __name__ == "__main__":
+    main()
---
+++ week2/test_main.py
@@ -0,0 +1,37 @@
+import unittest
+from week2.main import trace_wire, find_crossings, get_distance_manhattan, get_intersection_steps
+
+class Test(unittest.TestCase):
+    def test_trace_wire(self):
+        self.assertEqual(trace_wire(['U3']), [(0,0),(0,3)])
+        self.assertEqual(trace_wire(['D3']), [(0,0),(0,-3)])
+        self.assertEqual(trace_wire(['R3']), [(0,0),(3,0)])
+        self.assertEqual(trace_wire(['L10']), [(0,0),(-10,0)])
+        # combo
+        self.assertEqual(trace_wire(['R8','U5','L5','D3']), [(0,0),(8,0),(8,5),(3,5),(3,2)])
+        self.assertEqual(trace_wire(['U7','R6','D4','L4']), [(0,0),(0,7),(6,7),(6,3),(2,3)])
+    
+    def test_find_crossings(self):
+        # cross
+        self.assertEqual(find_crossings([(0,0),(0,3)],[(0,0),(-1,0),(-1,2),(4,2)]), [(0,2)])
+        # touch
+        self.assertEqual(find_crossings([(0,0),(3,0)],[(0,0),(0,4),(2,4),(2,0)]), [(2,0)])
+        # not touch
+        self.assertEqual(find_crossings([(0,0),(0,3)],[(0,0),(4,0),(4,2),(1,2)]), [])
+        # parallel
+        self.assertEqual(find_crossings([(0,0),(0,3)],[(0,0),(1,0),(1,4)]), [])
+    
+
+    def test_distance(self):
+        self.assertEqual(get_distance_manhattan((3,3)), 6)
+        self.assertEqual(get_distance_manhattan((-3,3)), 6)
+        self.assertEqual(get_distance_manhattan((-3,-3)), 6)
+        self.assertEqual(get_distance_manhattan((0,-3)), 3)
+    
+    def test_steps(self):
+        self.assertEqual(get_intersection_steps((2,2),[(0,0),(0,2),(2,2)],[(0,0),(2,0),(2,2)]), 8)
+        self.assertEqual(get_intersection_steps((2,2),[(0,0),(0,2),(4,2)],[(0,0),(2,0),(2,4)]), 8)
+        self.assertEqual(get_intersection_steps((0,-2),[(0,0),(4,0),(4,-2),(-4,-2)],[(0,0),(0,-2),]), 12)
+
+if __name__ == '__main__':
+    unittest.main()
