d614f31fc622acd29a57cbb85853547e4baa9a6d
Brady Field <emrysblack@gmail.com>
2022-10-24 17:06:44 -0500

    mark vars private

--- shared/image.py
+++ shared/image.py
@@ -2,15 +2,15 @@ class Image():
     def __init__(self, width, height):
         self.width = width
         self.height = height
-        self.dimensions = self.width * self.height
-        self._pixels: list = []
+        self._dimensions = self.width * self.height
+        self._pixels = []
         
     def load(self, data):
         if len(data) % (self.width * self.height) != 0:
             raise ValueError(f'Could not load image data to dimensions {self.width} x {self.height}')
         
-        self.data = data
-        self.layers = int(len(data) / (self.width * self.height))
+        self._data = data
+        self._layers = int(len(data) / (self.width * self.height))
         self.__get_display_data()
         return self
     
@@ -23,7 +23,7 @@ class Image():
         # define terminal color values for display
         colors = {'0': "\033[40m \033[0m", '1': "\033[47m \033[0m", '2': " "}
         # print image
-        for index in range(0, self.dimensions, self.width):
+        for index in range(0, self._dimensions, self.width):
             row = self._pixels[index:index + self.width]
             print(''.join([colors[pixel] for pixel in row]))
     
@@ -32,10 +32,10 @@ class Image():
         flattens loaded data into a single display layer
         """
         
-        for index in range(self.dimensions):
+        for index in range(self._dimensions):
             # get pixel value
-            for layer in range(self.layers):
-                if (pixel := self.data[layer * self.dimensions + index]) != "2":
+            for layer in range(self._layers):
+                if (pixel := self._data[layer * self._dimensions + index]) != "2":
                     self._pixels.append(pixel)
                     break
             else:
